3282371572111315305c964b7bdcdc47415ec8d1
mattegan111 <mnegan93@gmail.com>
2021-08-31 11:43:26 +1200

    Makes password a required Schema property

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const User = mongoose.model(
   "User",
   new Schema({
     username: { type: String, required: true },
-    password: { type: String }
+    password: { type: String, required: true }
   })
 );
 
